7a55d88de9251fe4d73e1d53278debe47f8eb921
Travis Madar <travismadar@gmail.com>
2019-05-25 20:13:01 -0700

    put space in the Form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Section from "../Section/Section";
+import styled from "styled-components";
+
+const FormContainer = styled.div`
+  margin-top: 25%;
+  margin-left: 10%;
+`;
 
 export default class Form extends React.PureComponent {
     render() {
         return (
-            <Section name={"Sleep"} />
+            <FormContainer>
+                <Section name={"Sleep"} />
+            </FormContainer>
         );
     }
 }
